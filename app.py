a4e514e77f30e73e4014a019b5e79b54cce51752
KKhvatova <krisxv1@mail.ru>
2020-03-11 17:52:27 +0100

    add 3rd and 4th page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from models import *
+from sqlalchemy import or_
 
 app = Flask(__name__)
 
@@ -36,7 +37,7 @@ def sing_in():
             return render_template('index.html', message='Please enter required fields')
         if db.session.query(User).filter(User.username == username, User.password == password).count() == 0:
             return render_template('index.html', message ='The email or password you entered is invalid')
-        return render_template('more.html')
+        return render_template('search.html')
 
 
 @app.route("/register")
@@ -62,6 +63,31 @@ def sing_up():
             db.session.add(data)
             db.session.commit()
             return render_template('success.html')
+
+@app.route("/result", methods = ["GET"])
+def search():
+    if request.method == 'GET':
+        isbn = request.args.get('isbn')
+        title = request.args.get('title')
+        author = request.args.get('author')
+        if isbn == '' and title == '' and author == '':
+            return render_template('search.html', message='Please enter al least one field')
+        if db.session.query(Book).filter(or_(Book.isbn == isbn, Book.title == title, Book.author == author)).count() == 0:
+            return render_template('search.html', message='There is no match')
+        else:
+            results = db.session.query(Book).filter(or_(Book.isbn == isbn, Book.title == title, Book.author == author))
+            return render_template('success.html', results=results)
+
+@app.route("/result/<int:book_id>")
+def book(book_id):
+    """List details about a single book."""
+
+    # Make sure flight exists.
+    book = db.session.query(Book).filter(Book.id == book_id).first()
+    if book is None:
+        return render_template("error.html", message="No such book.")
+    else:
+        return render_template("book_page.html", book=book)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -15,4 +15,19 @@ class User(db.Model):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+class Book(db.Model):
+
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(80), index=False, nullable=False)
+    title = db.Column(db.String(80), index=False, nullable=False)
+    author = db.Column(db.String(80), index=False, nullable=False)
+    year = db.Column(db.Integer, index=False, nullable=False)
+    
+    def __init__(self, isbn, title, author, year):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.year = year
         
